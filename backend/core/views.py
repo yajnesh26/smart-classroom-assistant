5c86b25db04a7e8e601fc0896bd0ffb75ca82c1b
yajnesh <ganigayajnesh26@gmail.com>
2026-02-22 00:06:50 +0530

    Create QR generator API

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,12 @@
 from rest_framework import viewsets
 from .models import Student, Attendance
 from .serializers import StudentSerializer, AttendanceSerializer
+import qrcode
+from io import BytesIO
+from django.http import HttpResponse
+from django.utils import timezone
+from datetime import timedelta
+from .models import AttendanceSession
 
 # Create your views here.
 class StudentViewSet(viewsets.ModelViewSet):
@@ -10,3 +16,17 @@ class StudentViewSet(viewsets.ModelViewSet):
 class AttendanceViewSet(viewsets.ModelViewSet):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
+
+def generate_qr(request):
+    session = AttendanceSession.objects.create(
+        expires_at=timezone.now() + timedelta(minutes=5)
+    )
+
+    qr_data = str(session.session_id)
+
+    img = qrcode.make(qr_data)
+
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+
+    return HttpResponse(buffer.getvalue(), content_type="image/png")
