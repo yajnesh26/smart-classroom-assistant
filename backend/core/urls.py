6b3fe72cb927aefac5e1319f1aaaeeca6740fb2d
yajnesh <ganigayajnesh26@gmail.com>
2026-02-22 21:57:13 +0530

    Add QR attendance system

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from .views import StudentViewSet, AttendanceViewSet
+from django.urls import path
+from .views import generate_qr, scan_qr
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet)
 router.register(r'attendance', AttendanceViewSet)
 
 urlpatterns = router.urls
+urlpatterns += [
+    path('generate_qr/', generate_qr),
+    path('scan/', scan_qr),
+]
