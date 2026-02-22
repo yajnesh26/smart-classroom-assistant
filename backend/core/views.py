5ea9431b75da75d89ff7f19792597499cdb0e067
yajnesh <ganigayajnesh26@gmail.com>
2026-02-22 21:21:04 +0530

    Add scan API

--- backend/core/views.py
+++ backend/core/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponse
 from django.utils import timezone
 from datetime import timedelta
 from .models import AttendanceSession
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 class StudentViewSet(viewsets.ModelViewSet):
@@ -30,3 +32,26 @@ def generate_qr(request):
     img.save(buffer, format="PNG")
 
     return HttpResponse(buffer.getvalue(), content_type="image/png")
+
+@api_view(['POST'])
+def scan_qr(request):
+    session_id = request.data.get('session_id')
+    student_id = request.data.get('student_id')
+
+    try:
+        session = AttendanceSession.objects.get(session_id=session_id)
+
+        if session.expires_at < timezone.now():
+            return Response({"error": "Session expired"}, status=400)
+        
+        student = Student.objects.get(id=student_id)
+
+        Attendance.objects.create(
+            student=student,
+            status="Present"
+        )
+
+        return Response({"message": "Attendance marked"})
+    
+    except AttendanceSession.DoesNotExist:
+        return Response({"error": "Invalid session"}, status=400)
