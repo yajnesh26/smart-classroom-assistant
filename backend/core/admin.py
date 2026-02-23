3466d3d5a2894c32b8fd696fb816446194db9ce0
yajnesh <ganigayajnesh26@gmail.com>
2026-02-23 11:15:23 +0530

    Register Task model in Admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Student, Attendance
 from .models import AttendanceSession
 from .models import Timetable
+from .models import Task
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Attendance)
 admin.site.register(AttendanceSession)
 admin.site.register(Timetable)
+admin.site.register(Task)
